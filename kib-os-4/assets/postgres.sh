da23f619a13de5e008348033e44fdf40f905e708
Епифанцев Алексей Александрович <AAEpifantsev@sberbank.ru>
2022-10-20 23:06:20 +0300

    kib-os-4

---
+++ kib-os-4/assets/postgres.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+PS1=1
+source /root/.bashrc
+
+OUT_FILE=~/connection.env
+DONE_FILE=/usr/local/etc/k8s.sh.done
+
+_user=student
+infra_project=infra
+work_project=work
+
+infra_context=${infra_project}-${_user}
+work_context=${work_project}-${_user}
+
+oc config use-context ${infra_context}
+curr_project=$(oc project -q)
+
+####deploy postgres
+pgpass=$(cat /proc/sys/kernel/random/uuid)
+oc create secret generic pg-postgresql --from-literal=postgres-password="$pgpass" --dry-run=client -oyaml \
+| oc apply -f-
+oc apply -f /usr/local/k8s -n "${curr_project}"
+pg_ip=$(oc get service postgresql -o template --template {{.spec.clusterIP}})
+sed "s/PG_IP/${pg_ip}/g" /usr/local/pg/connection.env >> $OUT_FILE
+####end
+
+oc config use-context ${work_context}
+touch $DONE_FILE
