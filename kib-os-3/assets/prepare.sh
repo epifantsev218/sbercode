80adc5bfe577a70fce42ae4af27655ae96b44d6d
Alexey Epifantsev <alexey.epifantsev@gmail.com>
2022-10-23 18:44:42 +0300

    kib-os-3 test

--- kib-os-3/assets/prepare.sh
+++ kib-os-3/assets/prepare.sh
@@ -23,11 +23,13 @@ task_dir=/root
 oc apply -f "${os_dir}/server.yml"
 # ingress
 sed "s/PROJECT_PLACEHOLDER/${curr_project}/g" "${template_dir}/ingress-params.env" >> "${os_dir}/ingress-params.env"
-oc process -f "${os_dir}/ingress-template.yml" --param-file "${os_dir}/ingress-params.env" -o yaml | oc apply -f
+oc process -f "${os_dir}/ingress-template.yml" --param-file "${os_dir}/ingress-params.env" -o yaml > "${os_dir}/conf.yml"
+oc apply -f "${os_dir}/conf.yml"
 # http
 easy_url="$(cat /proc/sys/kernel/random/uuid).apps.sbc-okd.pcbltools.ru"
 sed "s/EASY_URL_PLACEHOLDER/${easy_url}/g" "${template_dir}/easy-server-params.env" >> "${os_dir}/easy-server-params.env"
-oc process -f "${os_dir}/easy.yml" --param-file "${os_dir}/easy-server-params.env" -o yaml | oc apply -f
+oc process -f "${os_dir}/easy.yml" --param-file "${os_dir}/easy-server-params.env" -o yaml > "${os_dir}/conf.yml"
+oc apply -f "${os_dir}/conf.yml"
 # cert
 simple_url="$(cat /proc/sys/kernel/random/uuid).apps.sbc-okd.pcbltools.ru"
 mutual_url="$(cat /proc/sys/kernel/random/uuid).apps.sbc-okd.pcbltools.ru"
@@ -36,10 +38,12 @@ openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout "${os_dir}/key.pe
 oc create secret generic certs --from-file=key.pem="${os_dir}/key.pem" --from-file=crt.pem="${os_dir}/crt.pem" --from-file=ca.pem="${os_dir}/crt.pem"
 # simple TLS
 sed "s/SIMPLE_URL_PLACEHOLDER/${simple_url}/g" "${template_dir}/simple-server-params.env" >> "${os_dir}/simple-server-params.env"
-oc process -f "${os_dir}/simple.yml" --param-file "${os_dir}/simple-server-params.env" -o yaml | oc apply -f
+oc process -f "${os_dir}/simple.yml" --param-file "${os_dir}/simple-server-params.env" -o yaml > "${os_dir}/conf.yml"
+oc apply -f "${os_dir}/conf.yml"
 # mutual TLS
 sed "s/MUTUAL_URL_PLACEHOLDER/${mutual_url}/g" "${template_dir}/mutual-server-params.env" >> "${os_dir}/mutual-server-params.env"
-oc process -f "${os_dir}/mutual.yml" --param-file "${os_dir}/mutual-server-params.env" -o yaml | oc apply -f
+oc process -f "${os_dir}/mutual.yml" --param-file "${os_dir}/mutual-server-params.env" -o yaml > "${os_dir}/conf.yml"
+oc apply -f "${os_dir}/conf.yml"
 #client
 sed "s/EASY_URL_PLACEHOLDER/${easy_url}/g" "${template_dir}/easy-params.env" >> "${task_dir}/easy-params.env"
 sed "s/SIMPLE_URL_PLACEHOLDER/${simple_url}/g" "${template_dir}/simple-params.env" >> "${task_dir}/simple-params.env"
